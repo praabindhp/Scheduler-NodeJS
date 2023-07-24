c9a00460c2a8ccd1fc019803323d4405527a24ca
praabindh <praabindh@codingmart.com>
2023-07-24 12:53:04 +0530

    NodeJS Scheduler

--- scheduler.js
+++ scheduler.js
@@ -1,11 +1,20 @@
 const schedule = require("node-schedule");
 
+// Specified Date & Time
 const date = new Date(2023, 6, 24, 12, 10, 15);
 
 const job = schedule.scheduleJob(date, () => {
-  console.log("🎩🎩🎩");
+  console.log("🎩🎒🎓");
 });
 
-schedule.scheduleJob("*/3 * * * * *", () => {
+// Custom Rounds of Execution
+let i = 0;
+let n = 3;
+const job2 = schedule.scheduleJob("*/3 * * * * *", () => {
   console.count("Victory To The Blood Of Jesus");
+  i++;
+  if (i === n) {
+    console.log("🎩🎒🎓");
+    job2.cancel();
+  }
 });
